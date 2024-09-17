87741774037e71ba6c8f716d5e62ddd9b7772308
Bruno Soares Veríssimo <94240946+BrunoSoaresV@users.noreply.github.com>
2024-09-17 09:20:27 -0300

    Update intercalacaoBalanceada.h

--- intercalacaoBalanceada.h
+++ intercalacaoBalanceada.h
@@ -2,7 +2,6 @@
 #define INTERCALACAOBALANCEADA_H
 #include "dados.h"
 
-void gerarBlocosOrdenados(FILE* arquivoDeEntrada, int quantidade);
-void ordenaBlocoPorHeap(Dados alunos[], int n);
-void intercalacaoBalanceada(FILE *fitasDeEntrada[], FILE *fitasDeSaida[], int quantidadeBlocos);
+void gerarBlocosPorSelecaoSubstituicao(FILE* arquivoDeEntrada, int quantidade);
+void intercalacaoBalanceadaComSelecaoSubstituicao(FILE *fitasDeEntrada[], FILE *fitasDeSaida[], int quantidadeBlocos);
 #endif
